86662f3f99ad58c61b7c85ce2912b39947d3a0bc
DoniyorArtykbaev <donerartykbaev@gmail.com>
2024-06-04 08:26:23 +0500

    JS 5-dars
    Zeromax IT-Center 35-guruh

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//let headTag = document.head;
+//let bodyTag = document.body;
+//let havola = document.documentURI;
+//let formalar = document.forms;
+//let 
+
+//console.log(headTag);
+
+// function butonniBos(){
+//     alert("Siz button ni bostingiz!");
+// }
+
+// function buttoniIkkiBos(){
+//     alert("Siz button ni ikki marta bostingiz!");
+// }
+
+// function tanlash(event){
+//     alert("siz hozir " + event.target.value + "ni tanladingiz");
+// }
+
+// function inputgaBosing(){
+//     console.log("Inputga bosildi!");
+// }
+
+// function inpudanChiqish(){
+//     console.log("Inputdan chiqildi!")
+// }
+
+//function yozish(event){
+//    console.log(event.target.value);
+//}
+//document.querySelector(".text").addEventListener("copy",function(event))
+function harakatlanish(e){
+    console.log(e.clientX,e.clientY);
+}
+function kirish(){
+    console.log("Hududga kirildi!");
+}
+function chiqish(){
+    
+}
+
+
+
+
